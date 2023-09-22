b2e91bbdea697d41b24e7f7dda125a101a12ca07
Jorge Hernandez <jehernandezr@udistrital.edu.co>
2023-09-22 08:01:39 -0500

    Añadir tablero

--- candicrush/app/src/main/java/com/ud/candicrushud/BoardActivity.kt
+++ candicrush/app/src/main/java/com/ud/candicrushud/BoardActivity.kt
@@ -2,10 +2,32 @@ package com.ud.candicrushud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TableLayout
+import android.widget.TableRow
 
 class BoardActivity : AppCompatActivity() {
+
+    lateinit var boardLayout: TableLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board)
+
+        boardLayout = findViewById<TableLayout>(R.id.layoutBoard)
+        generateBoard()
+    }
+
+    fun generateBoard(){
+        for (row in 1..9){
+            var tableRow = TableRow(this)
+            for (col in 1..9){
+                var button = Button(this)
+                button.text = "$col"
+
+                tableRow.addView(button)
+            }
+
+            boardLayout.addView(tableRow)
+        }
     }
 }
---
+++ clase-espejo/back/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBb6JHGZPGQQN5Gv_BP5-uUjyAItNCGLhw",
+  authDomain: "usuarios-ud-2023-3.firebaseapp.com",
+  projectId: "usuarios-ud-2023-3",
+  storageBucket: "usuarios-ud-2023-3.appspot.com",
+  messagingSenderId: "812036611083",
+  appId: "1:812036611083:web:3a1d20a02df672c2c93b0e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
